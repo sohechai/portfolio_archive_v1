98860721ccd5d84a39d79357c8e5ecc64061e5aa
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro-2.local>
2022-10-14 20:46:20 +0200

    home OK about OK dark/light mode OK

--- portfolio_frontend/src/App.js
+++ portfolio_frontend/src/App.js
@@ -8,37 +8,41 @@ import NavBar from './components/NavBar';
 import logo from './images/logo.png'
 import { useState } from 'react';
 import { ThemeContext, themes } from './components/contexts/themeContext';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 function App() {
 	const [darkMode, setDarkMode] = useState(true);
 
 	return (
 		<BrowserRouter>
-		<Routes>
-			<Route path="/" exact element={<Home />}/>
-			<Route path="about" element={<About />} />
-			<Route path="skills" element={<Skills />} />
-			<Route path="work" element={<Work />} />
-		</Routes>
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="skills" element={<Skills />} />
+				<Route path="work" element={<Work />} />
+			</Routes>
 			<NavLink to="/">
-					<img src={logo}  alt="" className='logo'/>
+				<img src={logo} alt="" className='logo' />
 			</NavLink>
-			<ThemeContext.Consumer>
-            {({ changeTheme }) => (
-              <button
-                onClick={() => {
-                  setDarkMode(!darkMode);
-                  changeTheme(darkMode ? themes.light : themes.dark);
-                }}
-              >
-				<DarkModeIcon />
-                {/* <i className={darkMode ? 'fas fa-sun' : 'fas fa-moon'}></i>
-                <span className="d-lg-none d-md-block">Switch mode</span> */}
-              </button>
-            )}
-          </ThemeContext.Consumer>
+			<div className='lightingMode'>
+				<ThemeContext.Consumer>
+					{({ changeTheme }) => (
+						(darkMode ?
+								<LightModeIcon onClick={() => {
+									setDarkMode(!darkMode);
+									changeTheme(darkMode ? themes.light : themes.dark);
+								}} />
+							:
+								<NightsStayIcon onClick={() => {
+									setDarkMode(!darkMode);
+									changeTheme(darkMode ? themes.light : themes.dark);
+								}}
+								/>
+						)
+					)}
+				</ThemeContext.Consumer>
+			</div>
 			<NavBar />
 		</BrowserRouter>
 	);
