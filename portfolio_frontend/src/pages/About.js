a39ed025d38998793729623ab7a4c5f476dae27b
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro-2.local>
2022-11-14 15:25:37 +0100

    fix text-size navbar OK fix text about OK

--- portfolio_frontend/src/pages/About.js
+++ portfolio_frontend/src/pages/About.js
@@ -13,9 +13,9 @@ function About() {
 				After 2 years I decided to reorient myself as a developer.
 				In 2019, I joined 42 school. This is where it all started.
 				I started to love creating with a keyboard, especially website. <br /><br />
-				{/* This is why I am looking for an internship of 4 or 6 months as  
-			soon as possible. This will allow me to learn and evolve with the help 
-			of senior developper.  */}
+				This is why I am looking for an internship of 4 or 6 months as  
+				soon as possible. This will allow me to learn and evolve with the help 
+				of senior developper. 
 			</p>
 			<a className='resume' href={resume} download="resume_Sofia_Hechaichi">DOWNLOAD MY RESUME</a>
 		</section>
