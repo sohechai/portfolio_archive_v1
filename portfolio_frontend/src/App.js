e24507b359dee111b0aa412513456d9a25f7408a
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro-2.local>
2023-02-01 18:11:41 +0100

    new logo

--- portfolio_frontend/src/App.js
+++ portfolio_frontend/src/App.js
@@ -13,14 +13,30 @@ import Contact from './pages/Contact';
 
 function App() {
 	const [darkMode, setDarkMode] = useState(true);
-	
+
 	return (
 		<BrowserRouter>
 			<section className='container'>
 				<section className='header'>
-					<NavLink to="/">
-						<img src={logo} alt="" className='logo' />
-					</NavLink>
+					<div className='logo'>
+						<NavLink to="/">
+							<svg xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 1000 1000">
+								<path class="letterS"
+									fill="none" stroke="#FCFCEF" strokeMiterlimit="10" strokeWidth="30"
+									d="M 522.00,397.50
+									C 516.00,376.50 498.49,357.93 468.00,369.00
+									84.80,493.14 843.68,698.04 463.50,844.50
+									248.36,891.75 534.00,561.00 601.50,526.50" />
+
+								<path class="letterH" fill="none" stroke="#FCFCEF" strokeMiterlimit="10" strokeWidth="30"
+									d="M 619.50,304.50
+									C 606.00,358.50 607.91,840.15 618.00,838.50
+									638.49,481.32 832.20,503.83 813.00,838.50" />
+							</svg>
+							<div className="ball" />
+						</NavLink>
+					</div>
 					<div className='lightingMode'>
 						<ThemeContext.Consumer>
 							{({ changeTheme }) => (
@@ -49,7 +65,7 @@ function App() {
 						<Route path="contact" element={<Contact />} />
 					</Routes>
 					<div className='signature'>
-						<p>&#169; designed and developed by Sofia Hechaïchi with ReactJS.</p>
+						<p>&#169; 2023 Sofia Hechaïchi. Made with ReactJS.</p>
 					</div>
 				</section>
 			</section>
