23105867e1e19fa3d13a113f4e1c62e8325b87e6
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro-2.local>
2023-02-06 18:19:22 +0100

    fix update theme OK

--- portfolio_frontend/src/App.js
+++ portfolio_frontend/src/App.js
@@ -8,21 +8,14 @@ import { useEffect, useState } from 'react';
 import Contact from './pages/Contact';
 
 function App() {
-	const initialDarkMode = localStorage.getItem("darkMode") === "true" ? false : true;
-	
-	// if (!localStorage.getItem("darkMode")) {
-	// 	localStorage.setItem("darkMode", initialDarkMode.toString());
-	// }
-	const [darkMode, setDarkMode] = useState(initialDarkMode);
+	const [darkMode, setDarkMode] = useState(true);
 
 	const setThemeMode = () => {
 		setDarkMode(!darkMode);
 		if (darkMode === true) {
 			document.documentElement.setAttribute('data-theme', 'dark')
-			localStorage.setItem('darkMode', 'true');
 		} else {
 			document.documentElement.setAttribute('data-theme', 'light');
-			localStorage.setItem('darkMode', 'false');
 		}
 	}
 
@@ -32,30 +25,26 @@ function App() {
 		} else {
 			document.documentElement.setAttribute('data-theme', 'light');
 		}
-	}, []);
-
-	useEffect(() => {
-		localStorage.setItem("darkMode", darkMode.toString());
 	}, [darkMode]);
 
 	return (
 		<BrowserRouter>
 			<section className='container'>
-				<div className={`toggletheme ${darkMode ? '' : 'active'}`} onClick={setThemeMode} />
+				<div className={'toggletheme'} onClick={setThemeMode} />
 
 				<section className='header'>
 					<div className='logo'>
 						<NavLink to="/">
 							<svg xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 1000 1000">
-								<path class="letterS"
+								<path className="letterS"
 									fill="none" strokeMiterlimit="10" strokeWidth="30"
 									d="M 522.00,397.50
 									C 516.00,376.50 498.49,357.93 468.00,369.00
 									84.80,493.14 843.68,698.04 463.50,844.50
 									248.36,891.75 534.00,561.00 601.50,526.50" />
 
-								<path class="letterH" fill="none" strokeMiterlimit="10" strokeWidth="30"
+								<path className="letterH" fill="none" strokeMiterlimit="10" strokeWidth="30"
 									d="M 619.50,304.50
 									C 606.00,358.50 607.91,840.15 618.00,838.50
 									638.49,481.32 832.20,503.83 813.00,838.50" />
